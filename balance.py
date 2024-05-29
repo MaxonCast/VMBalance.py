7a454dcd252233d9551fd0f7ed2a224ac0e73bbf
U-HSC\mcasteres <mcasteres@LE1714.HSC.LOC>
2024-05-29 15:59:51 +0200

    29/05/2024:save

--- balance.py
+++ balance.py
@@ -1,7 +1,19 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
+hostname = "https://mwa"
 username = "mwa"
-password = "celui-dont-on-ne-doit-pas-prononcer-le-nom"
+password = "mdp"
 
 VMlist = []
 
-print (username, password)
+print (username,password)
+
+def auth_vcenter(user,userpass):
+	resp = requests.post('{}/com/vmware/cis/session'.format(api_url),auth=(username, password),verify=False)
+	if resp.status_code != 200:
+		print('Error {}'.format(resp.status_code))
+		return
+	return resp.json()['value']
