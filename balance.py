5961b5c84d8f9fb32172355a8d3483c48108a290
U-HSC\mcasteres <mcasteres@LE1714.HSC.LOC>
2024-05-30 11:01:44 +0200

    getting Auth-Token

--- balance.py
+++ balance.py
@@ -2,25 +2,25 @@ import requests
 from requests.auth import HTTPBasicAuth
 import json
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+import getpass
 
-hostname = "ici"
-username = "mwa"
-password = "no"
+hostname = input("hostname : ")
+username = input("username : ")
+password = getpass.getpass("password : ")
 
 VMlist = []
 
-print (username)
-
-def auth_vcenter(user,userpass):
-	resp = requests.post('https://{}/api/session',auth=(username, password))
-	if resp.ok:
+# GET THE TOKEN
+# curl -k -X POST https://vcsa7.hsc.loc/rest/com/vmware/cis/session -u username:password
+def auth_vcenter():
+	response = requests.post('https://{}/rest/com/vmware/cis/session'.format(hostname), verify=False, auth=(username,password))
+	if response.ok:
 		sessionID = response.json()
-		print(sessionID)
+		print("------",sessionID,"------")
 	else:
 		print("Erreur")
-#	if resp.status_code != 200:
-#		print('Error {}'.format(resp.status_code))
-#		return
-#	return resp.json()['value']
 
-auth_vcenter("moi","moi")
+# GET VM
+# curl -X GET 'https://vcsa7.hsc.loc/api/vcenter/vm' -H 'vmware-api-session-id: <ID>'
+
+auth_vcenter()
