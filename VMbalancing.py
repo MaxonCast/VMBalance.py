b7ceb9624ad81c1d285c91accb2a6774aa68345f
MaxonCast <maxon.casteres@gmail.com>
2024-06-05 16:07:12 +0200

    Printing Test

--- VMbalancing.py
+++ VMbalancing.py
@@ -1,5 +1,5 @@
 from getpass import getpass
-from pyVim.connect import SmartConnect, Disconnect
+from pyVim.connect import SmartConnect  # , Disconnect
 from pyVmomi import vim
 
 
@@ -8,24 +8,39 @@ from pyVmomi import vim
 # password = getpass(prompt="Password : ")
 
 
-# Connection au VCenter
+# Connecting to VCenter
 def authVSphere():
-    content = "vide"
+    result = "vide"
     service_instance = SmartConnect(host=getpass(prompt="Hostname : "), user=getpass(prompt="Username : "),
                                     pwd=getpass(prompt="Password : "))
     if not service_instance:
         raise Exception("Failed to connect to vCenter Server")
 
     try:
-        content = service_instance.RetrieveContent()
+        result = service_instance.RetrieveContent()
     except Exception as e:
         print("Error: {}".format(str(e)))
-    finally:
-        # Disconnect from the vCenter server
-        Disconnect(service_instance)
-    return content
+    return result
 
 
 content = authVSphere()
 print("------ RESULTS ------")
 print(content)
+
+
+# Getting VM list
+def getVM(vcenter):
+    container = vcenter.rootFolder
+    view_type = [vim.VirtualMachine]
+    recursive = True
+    container_view = vcenter.viewManager.CreateContainerView(container, view_type, recursive)
+    children = container_view.view
+    return children
+
+
+VM_List = getVM(content)
+print("------ VM LIST ------")
+for VM in VM_List:
+    print(VM, vim.VirtualMachine.summary.guestFullName)
+
+# Disconnect(service_instance)
